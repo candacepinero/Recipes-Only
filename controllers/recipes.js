7d9cfa0f2f5f919374f848485ba43159b057fecc
candace pinero <candcae.pinero@gmail.com>
2022-05-29 20:49:09 -0400

    wp: create route and home page working

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -6,20 +6,33 @@ const router = express.Router()
 //Routes
 //Index/Homepage
 router.get('/', (req, res) => {
-    res.send('this route is working')
-})
+    Recipe.find({}, (err, foundRecipes)=> {
+        res.render('recipes/index.ejs', {recipes: 
+        foundRecipes,
+        });
+    });
+});
 
 //New/add new recipe
+router.get('/new', (req, res) => {
+   res.render('recipes/new.ejs')
+})
 
 //Delete
 
 //Update
 
 //Create
-
+router.post('/', (req, res) => {
+    Recipe.create(req.body, (err, createdRecipe) => {
+        res.redirect('/recipes')
+    });
+});
 //Edit
 
 //Show/view recipe
-
+// router.get('/:id', (req, res) => {
+    
+// })
 
 module.exports = router;
